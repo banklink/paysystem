ddf8cb7a97b3ff4a3cfc6965d0b5caef16625f4c
banklink <banklink@yeah.net>
2018-02-28 17:01:36 +0800

    Improve the initiation payment and return payment result processing.

--- app/src/main/java/com/banklink/paysystem/MainActivity.java
+++ app/src/main/java/com/banklink/paysystem/MainActivity.java
@@ -1,14 +1,8 @@
 package com.banklink.paysystem;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-import com.banklink.lib.BLPay;
-import com.banklink.lib.config.ResultInfo;
-import com.banklink.lib.listener.BLPayListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,40 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btn_pay).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BLPay.pay("10003","1","www.baidu.com",MainActivity.this);
-            }
-        });
-
-        findViewById(R.id.btn_time).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        BLPay.payResult(requestCode, resultCode, data, new BLPayListener() {
-            @Override
-            public void paySuccess(ResultInfo result) {
-                Log.e("支付结果", "支付回调成功"+result);
-            }
-
-            @Override
-            public void payCancel() {
-                Log.e("支付结果", "支付回调取消");
-            }
-
-            @Override
-            public void payFailure() {
-                Log.e("支付结果", "支付回调失败");
-            }
-        });
-    }
 }
