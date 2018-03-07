690953b0002443415bbb220660d4c612d693f308
banklink <banklink@yeah.net>
2018-03-07 09:45:43 +0800

    A demonstration method is added to directly run the program to see the effect.

--- app/src/main/java/com/banklink/paysystem/App.java
+++ app/src/main/java/com/banklink/paysystem/App.java
@@ -3,6 +3,7 @@ package com.banklink.paysystem;
 import android.app.Application;
 
 import com.banklink.lib.BLPay;
+import com.banklink.lib.config.ConfigInfo;
 
 /**
  * Author：FynnJason
@@ -14,6 +15,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ConfigInfo.TEST = "1";
         BLPay.getInstance().init(this,"68ccdfadf66affab10ecdfc90abf8c62","7d546ddd9223cfasf5df0e124266b092","6fa5616gg3ac92228a9dd9a0be5cac9f");
     }
 }
